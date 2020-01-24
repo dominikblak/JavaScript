26337865a6936f04b769c7d6408dd05cef25c146
Dominik Blak <bldominikak@gmial.com>
2020-01-24 18:09:38 +0100

    for

--- 3.Font grow/main.js
+++ 3.Font grow/main.js
@@ -1,1 +1,11 @@
-// rozwiązanie
+const btn = document.querySelector("button");
+const liItems = document.querySelectorAll("li");
+let font = 10;
+
+btn.addEventListener("click", function() {
+  font++;
+  for (let i = 0; i < liItems.length; i++) {
+    liItems[i].style.display = "block";
+    liItems[i].style.fontSize = `${font}px`;
+  }
+});
