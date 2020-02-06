ef35a4a6cf0384a4b89a6a70f295cc6f8b0b7f18
Dominik Blak <bldominikak@gmial.com>
2020-02-06 20:02:26 +0100

    offset

---
+++ 5a.falling square/main.js
@@ -0,0 +1,29 @@
+const div = document.querySelector("div");
+let divX = 150;
+let divY = 50;
+div.style.left = divX + "px";
+div.style.top = `${divY}px`;
+
+let drawActive = false;
+let insertDivX;
+let insertDivY;
+
+div.addEventListener("mousedown", e => {
+  div.style.backgroundColor = "gray";
+  drawActive = !drawActive;
+  insertDivX = e.offsetX;
+  insertDivY = e.offsetY;
+  console.log(insertDivX, insertDivY);
+});
+document.addEventListener("mousemove", e => {
+  if (drawActive) {
+    divX = e.clientX - insertDivX;
+    divY = e.clientY - insertDivY;
+    div.style.left = `${divX - 50}px`;
+    div.style.top = `${divY - 50}px`;
+  }
+});
+div.addEventListener("mouseup", () => {
+  div.style.backgroundColor = "black";
+  drawActive = !drawActive;
+});
