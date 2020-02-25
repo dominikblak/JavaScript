1570f8dc0f3ac498af97a272963551cbbee54467
Dominik Blak <bldominikak@gmial.com>
2020-02-25 18:26:13 +0100

    forEach

---
+++ 7d.Password-forEach/main.js
@@ -0,0 +1,27 @@
+// Projekt 5 - Zadanie dla Ciebie
+// Sprawdź czy wpisane w input hasło pasuje do któegoś z hasel w tablicy. Jeśli tak wyświetl wiadomość w <div>
+//wielkość liter nie ma znaczenia (wykorzystaj metodę .toUpperCase lub .toLowerCase - pamiętaj, że metody te działają na pojedyńczym elemencie typu string)
+
+const input = document.querySelector("input");
+const passwords = ["jedEN", "DwA"];
+const messages = ["super", "działa!"];
+const div = document.querySelector("div");
+
+passwords.forEach((password, index) => {
+  passwords[index] = password.toLowerCase();
+});
+
+const showMessage = e => {
+  //tutaj rozwiązanie
+  // PS. nie skupiaj się na niczym innym w zadaniu niż porównanie i wyświetlenie ( nie rób czyszczenia inputa itp. nie są potrzebne, chyba że masz ochotę)
+  const text = e.target.value.toLowerCase();
+  div.textContent = "";
+  passwords.forEach((input, index) => {
+    if (input === text) {
+      div.textContent = messages[index];
+      e.target.value = "";
+    }
+  });
+};
+
+input.addEventListener("input", showMessage);
